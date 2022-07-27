57f89cef6c412740cce6c26b3d43796f64da85d2
CristiMacovei <cristimacovei26@gmail.com>
2022-07-27 14:43:32 +0300

    tailwind css init

---
+++ pages/index.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className='mt-10 flex flex-row items-center justify-center'>
+      <span className='underline cursor-pointer text-lg'>Hello with tailwind css!</span>
+    </div>
+  );
+};
